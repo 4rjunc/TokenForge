ba2e644790ad51d6b205b1563bc7483bf9b11820
AaroneGeorge <aaroneofficial1879@gmail.com>
2025-07-30 12:43:28 +0530

    apk for mundu

---
+++ app/src/main/java/app/vercel/token_forge_delta/twa/DelegationService.java
@@ -0,0 +1,14 @@
+package app.vercel.token_forge_delta.twa;
+
+
+
+public class DelegationService extends
+        com.google.androidbrowserhelper.trusted.DelegationService {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        
+    }
+}
+
